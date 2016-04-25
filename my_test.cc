3b3d607cce6e797ba6c08ff51b7c4079cec93d94
Pei Xingxin <xingxinpei@gmail.com>
2016-04-25 14:37:00 +0800

    add fist test

---
+++ my_test.cc
@@ -0,0 +1,19 @@
+// =============================================================================
+//
+//       Filename:  gtest_test.h
+//
+//    Description:  `
+//
+//        Version:  1.0
+//        Created:  04/24/2016 06:06:39 PM
+//       Revision:  $Id$
+//       Compiler:  g++
+//
+//         Author:  michaelpei (Pei Xingxin), michaelpei@tencent.com
+//
+// =============================================================================
+#include "gtest/gtest.h"
+
+TEST(FactorialTest, Negative) {
+  EXPECT_EQ(1, 1);
+}
